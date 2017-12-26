67da891618c892c042c38cadef4c338e333fadae
bdion-nessit <bdion@nessit.net>
2017-12-26 13:33:37 -0500

    Swapped default template to use no-sidebar by default

--- page.php
+++ page.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * The template for displaying all pages
  *
@@ -8,32 +8,23 @@
 get_header(); 
 do_action('joints_before_content');
 ?>
-	
+			
 	<div class="content">
 	
 		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-			
-		<?php 
-            do_action('joints_secondary_sidebar');
- 		?>
 	
-		    <main class="main large-<?php echo $column_width; ?> medium-<?php echo $column_width; ?> small-12 cell" role="main">
+		    <main class="main small-12 medium-12 large-12 cell" role="main">
 				
-				<?php 
-					do_action('joints_entry'); //Default functionality found in functions/core.php
-				?>								
-			    					
-			</main> <!-- end #main -->
-
 			<?php 
-				do_action('joints_primary_sidebar'); //Main sidebar loads here by default
-			?>
+				do_action('joints_entry'); 
+			?>							
+
+			</main> <!-- end #main -->
 		    
 		</div> <!-- end #inner-content -->
-
+	
 	</div> <!-- end #content -->
 
 <?php 
 do_action('joints_after_content');
 get_footer(); 
-?>
